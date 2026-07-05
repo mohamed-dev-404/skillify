3fa8be04c6b8b78fcd71be34730a6f099c962181
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-07-05 19:47:50 +0300

    chore: add AppTextFormField and PasswordTextFormField for enhanced input handling

---
+++ lib/core/widgets/inputs/app_text_form_field.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class AppTextFormField extends StatelessWidget {
+  const AppTextFormField({
+    super.key,
+    this.hintText,
+    this.keyboardType,
+    this.validator,
+    this.prefixIcon,
+    this.readOnly = false,
+    this.onTap,
+    this.focusNode,
+    this.onChange,
+    this.textInputAction,
+    this.controller,
+  });
+  final String? hintText;
+  final TextInputType? keyboardType;
+  final String? Function(String?)? validator;
+  final Widget? prefixIcon;
+  final bool readOnly;
+  final Function()? onTap;
+  final Function(String)? onChange;
+  final FocusNode? focusNode;
+  final TextInputAction? textInputAction;
+  final TextEditingController? controller;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      readOnly: readOnly,
+      focusNode: focusNode,
+      textInputAction: textInputAction,
+      onTapOutside: (event) {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+      ),
+      validator: validator,
+      onChanged: onChange,
+      onTap: onTap,
+    );
+  }
+}
