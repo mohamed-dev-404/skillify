89d5c599fdb41c8a2f2b9778640c36dd62a8e0c1
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-07-05 19:47:28 +0300

    chore: add MainButton widget for customizable button implementation

---
+++ lib/core/widgets/buttons/main_button.dart
@@ -0,0 +1,39 @@
+import 'package:skillify/core/utils/colors/app_colors.dart';
+import 'package:skillify/core/utils/styles/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class MainButton extends StatelessWidget {
+  const MainButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.bgColor = AppColors.primary,
+    this.borderColor,
+    this.minWidth = double.infinity,
+    this.minHeight = 56,
+    this.textColor = AppColors.backgroundNormal,
+  });
+  final String text;
+  final Function() onPressed;
+  final Color bgColor;
+  final Color? borderColor;
+  final double minWidth;
+  final double minHeight;
+  final Color textColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: bgColor,
+        padding: EdgeInsets.zero,
+        maximumSize: Size(minWidth, minHeight),
+        minimumSize: Size(minWidth, minHeight),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        side: borderColor != null ? BorderSide(color: borderColor!) : null,
+      ),
+      onPressed: onPressed,
+      child: Text(text, style: AppStyles.bold14.copyWith(color: textColor)),
+    );
+  }
+}
