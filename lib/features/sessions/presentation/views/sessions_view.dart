4aca4ebbefb47aea77de9b5384eae2bf5b6c4deb
MohamedIbrahemHamad <mohammedhamad.dev@gmail.com>
2026-07-10 15:12:23 +0300

    feat: Add initial models, repositories, cubits, states, and views for explore, profile, sessions, and wallet features

---
+++ lib/features/sessions/presentation/views/sessions_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SessionsView extends StatelessWidget {
+  const SessionsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text('Sessions'),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/wallet/presentation/views/wallet_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WalletView extends StatelessWidget {
+  const WalletView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text('Wallet'),
+        ),
+      ],
+    );
+  }
+}
