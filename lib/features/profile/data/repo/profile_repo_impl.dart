7c059c3ef1f938e56b2c2ccf4f9162e8aceb89ac
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-07-15 01:23:18 +0300

    feat(profile): Implement ProfileRepo and ProfileRepoImpl for profile data handling

--- lib/features/profile/data/repo/profile_repo_impl.dart
+++ lib/features/profile/data/repo/profile_repo_impl.dart
@@ -1,1 +1,20 @@
-﻿// profile repo impl
+import 'package:dartz/dartz.dart';
+import 'package:skillify/core/services/network/api_consumer.dart';
+import 'package:skillify/features/profile/data/models/profile_mock.dart';
+import 'package:skillify/features/profile/data/models/profile_model.dart';
+import 'package:skillify/features/profile/data/repo/profile_repo.dart';
+
+class ProfileRepoImpl implements ProfileRepo {
+  final ApiConsumer api;
+
+  const ProfileRepoImpl(this.api);
+
+  @override
+  Future<Either<String, ProfileModel>> getProfile() async {
+    // Simulate network delay
+    await Future.delayed(const Duration(milliseconds: 500));
+    
+    // Return mock data for UI testing
+    return Right(ProfileMock.mockProfile);
+  }
+}
