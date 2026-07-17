8b2d6bff3120e8d0274d247589273fcae7361a0d
sohaib-khalifa <sohaipkhalifa47@gmail.com>
2026-07-18 01:46:30 +0300

    feat(profile): add EditProfileView and related widgets for profile editing

--- lib/features/profile/edit_profile/views/edit_profile_view.dart
+++ lib/features/profile/edit_profile/views/edit_profile_view.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skillify/core/di/service_locator.dart';
-import 'package:skillify/features/profile/my_profile/view_model/profile_cubit/profile_cubit.dart';
-import 'package:skillify/features/profile/my_profile/views/widgets/profile_view_body.dart';
+import 'package:skillify/features/profile/data/models/profile_model.dart';
+import 'package:skillify/features/profile/edit_profile/view_model/edit_profile_cubit/edit_profile_cubit.dart';
+import 'package:skillify/features/profile/edit_profile/views/widgets/edit_profile_view_body.dart';
 
-class ProfileView extends StatelessWidget {
-  const ProfileView({super.key});
+class EditProfileView extends StatelessWidget {
+  final ProfileModel profile;
+
+  const EditProfileView({super.key, required this.profile});
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => getIt<ProfileCubit>(),
-      child: const SafeArea(
-        child: ProfileViewBody(),
+      create: (context) => getIt<EditProfileCubit>()..init(profile),
+      child: const Scaffold(
+        body: SafeArea(
+          child: EditProfileViewBody(),
+        ),
       ),
     );
   }
